2cb9f5019e716c210985858f8174262b7f61ae64
dieuaime-shami <shamidieuaime272@gmail.com>
2025-08-19 01:21:26 +0200

    CardSurfaceLaptop

--- src/components/Microsoft.jsx
+++ src/components/Microsoft.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import MicrosoftLogo from '../assets/microsoft.png'
 import frontPageImage from '../assets/fontImage.webp'
 import surfaceDevice from '../assets/Surface-Devices.svg'
+import XboxGames from '../assets/XboxGames.svg'
+import IconsAccessories from '../assets/IconsAccessories.svg'
+import IconsBusiness from '../assets/IconsBusiness.svg'
+import IconPC from '../assets/IconPC.svg'
+import Microsoft365 from '../assets/Microsoft365.svg'
+import CardCopilot from '../assets/CardCopilot.avif'
+import CardSurfaceLaptop from '../assets/CardSurfaceLaptop.avif'
+
 
 
 const Microsoft = () => {
   return (
 <div>
-  <nav className='h-24 flex justify-around items-center'>
+  <nav className='h-24 flex justify-around items-center w-154'>
     <div className='flex gap-6 items-center'>
       <div><i class="fa-solid fa-bars fa-2xl" style={{fontSize:"2rem"}}></i></div>
       <div><i class="fa-solid fa-magnifying-glass" style={{fontSize:"1rem"}}></i></div>
@@ -22,34 +30,77 @@ const Microsoft = () => {
     </div>
   </nav>
   {/* first session */}
-  <div className='h-34 bg-green-200'>
+  <div className='h-28 bg-green-200 w-154'>
     <h2 className='text-2xl text-center font-semibold pt-5 '>Get school tech for less.If you don't love it, return it,</h2>
     <h2 className='text-2xl text-center font-semibold'>with 60-day free returns.</h2>
   </div>
  {/* font image */}
- <div>
+ <div className='w-154'>
    <img src={frontPageImage} alt="" />
  </div>
 
-    <div className='p-3'>
-      <h1 className='text-[33px] font-semibold'>Students get 3 free months of Microsoft 363</h1>
-      <p className='text-xl my-5'>Save for a limited on Microsoft 365 Personal with Copilot -featuring premium apps,cloud storage, and more -when you sign up with your college email address.</p>
+    <div className='p-3 w-154'>
+      <h1 className='text-[35px] font-semibold'>Students get 3 free months of Microsoft 363</h1>
+      <p className='text-2xl my-5'>Save for a limited on Microsoft 365 Personal with Copilot -featuring premium apps,cloud storage, and more -when you sign up with your college email address.</p>
       <button className='bg-blue-800 text-2xl py-3 px-4 text-white'>Learn more</button>
 
-      <hr className='border-t border-black my-5'/>
-      <div className='w-1/2 flex items-center justify-around mx-auto my-15'>
+      <hr className='border-t border-black my-5 w-150'/>
+      <div className='w-90 flex items-center justify-around mx-auto my-15 '>
         <i class="fa-regular fa-circle-pause fa-2xl"></i>
         <i class="fa-solid fa-less-than fa-2xl"></i>
         <button className='h-5 w-5 border border-black rounded-full'></button>
         <button className='h-5 w-5 border border-black rounded-full bg-black'></button>
         <i class="fa-solid fa-greater-than fa-2xl"></i>
       </div>
-      <div>
-        <div>
 
+      {/* Shopping */}
+      <div className='grid grid-cols-2  w-140 mx-auto gap-2'>
+        <div className='flex flex-col justify-center items-center'>
+         <img src={surfaceDevice} alt="surface Device" className='w-16 text-center'/>
+         <p className='text-lg text-blue-600 underline font-bold'>Shop Surface device</p>
         </div>
-        <div>
-          <i class="fa-solid fa-computer"></i>
+        <div className='flex flex-col justify-center items-center'>
+         <img src={XboxGames} alt="surface Device" className='w-16'/>
+         <p className='text-lg text-blue-600 underline font-bold'>Shop Xbox games and consoles</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+         <img src={IconsAccessories} alt="surface Device" className='w-16'/>
+         <p className='text-lg text-blue-600 underline font-bold'>Shop for accessories</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+         <img src={IconsBusiness} alt="surface Device" className='w-16'/>
+         <p className='text-lg text-blue-600 underline font-bold'>Shop for business</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+         <img src={IconPC} alt="surface Device" className='w-16'/>
+         <p className='text-lg text-blue-600 underline font-bold'>Find your next PC</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+         <img src={Microsoft365} alt="surface Device" className='w-16'/>
+         <p className='text-lg text-blue-600 underline font-bold'>Choose your Microsoft 365</p>
+        </div>
+      </div>
+
+      {/* shoping image */}
+      <div className='w-140 my-10 mx-auto border border-gray-300 shadow-xl'>
+        <img src={CardCopilot} alt="CardCopilot" className='w-full'/>
+        <div className='ml-6'>
+          <h1 className='text-3xl font-semibold my-2'>Copilot is your AI companion</h1>
+          <h1 className='text-xl'>Always by your side, ready to support you whenever and</h1>
+          <h1 className='text-xl'>whener you need it</h1>
+          <button className='bg-blue-600 text-2xl py-3 px-4 text-white my-6'>Download the Copilot app</button>
+        </div>
+      </div>
+
+      {/* CardSurfaceLaptop */}
+      <div className='w-140 mx-auto shadow-xl border border-gray-300 my-10'>
+        <img src={CardSurfaceLaptop} alt="" className='w-full '/>
+        <div className='w-130 mx-auto '>
+          <p className='text-3xl font-semibold my-3'>Trade in and get up to $500 cash back</p>
+          <p className='text-xl'>
+            Get extra cah back , plus a 3-month trial of Microsoft 365 when you purchase a select new Surface
+            Laptop,Copilot+ PC and trade in an eligible device. Offer ends 9/30.
+          </p>
         </div>
       </div>
     </div>
